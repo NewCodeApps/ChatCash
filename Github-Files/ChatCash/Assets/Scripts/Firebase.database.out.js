3f45cb0bdf812fdabd1efa4d1913e146e82e18c7
Brenden <Brendenstahle@gmail.com>
2020-05-16 17:54:33 -0500

    Add Github To this

---
+++ Github-Files/ChatCash/Assets/Scripts/Firebase.database.out.js
@@ -0,0 +1,6 @@
+// Get the Database service for the default app
+var defaultDatabase = firebase.database();
+
+// Get a reference to the root of the Database
+var rootRef = firebase.database().ref();
+
