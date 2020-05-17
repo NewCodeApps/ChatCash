9162e2561222d4baa7a93ff19a2dd482158d878c
Brenden <Brendenstahle@gmail.com>
2020-05-16 19:00:33 -0500

    update

--- gitsync.bat
+++ gitsync.bat
@@ -2,9 +2,3 @@ git add *
 git commit -m "Auto Commit Github"
 git pull ChatCash-Github master
 git push ChatCash-Github master
-
-git add *
-git commit -m "Auto Commit Gitlab"
-git pull origin master
-git push origin master
-pause
